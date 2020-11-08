ffc7c7272816c21336f3edf70fb161ecccfdf549
Shamil Gusseynov <42405526+Shamil-G@users.noreply.github.com>
2020-11-08 14:42:58 +0600

    Add files via upload

--- ads1115.c
+++ ads1115.c
@@ -25,12 +25,11 @@ uint8_t cmd_value[] = { APR_CONF_REG, // select config register
 
 
 int8_t init_ads1115(I2C_HandleTypeDef hi, uint8_t DEV_ADDR){
-	// Select CHIP where DEV_ADDR select from ADDR_A0, ADDR_A1, ADDR_A2, ADDR_A3
-	// and select Config register
-	// and configure Config Register
+	// first byte  - DEV_ADDR, it is equal - ADDR_A0, ADDR_A1, ADDR_A2, ADDR_A3
+	// second byte - select Config register - 0x01 = APR_CONF_REG
+	// third byte  - mode conversion
+	// fourth byte - SPS  and COMPARATOR mode
 	precision = ((cmd_value[1] & 0x0f)==UP_6V)?187.5:
-			    // Для шкалы 4V
-		    	//((cmd_value[1] & 0x0f)==0x02)?187.5:
 			    ((cmd_value[1] & 0x0f)==UP_4V)?125:
 			    ((cmd_value[1] & 0x0f)==UP_2V)?62.5:
 			    ((cmd_value[1] & 0x0f)==UP_1V)?31.25:
@@ -46,19 +45,18 @@ int8_t init_ads1115(I2C_HandleTypeDef hi, uint8_t DEV_ADDR){
 int16_t read_value_ads1115(I2C_HandleTypeDef hi, uint8_t DEV_ADDR){
 	// Select CHIP where DEV_ADDR select from ADDR_A0, ADDR_A1, ADDR_A2, ADDR_A3
 	// and select Conversation register
-	ads_value=0x55;
+	ads_value=0;
 	unsigned char recieve_buffer[2];
 	uint16_t new_addr = (uint16_t)DEV_ADDR | 0x01;
 
 	recieve_buffer[0]=0x00;
 	while(HAL_I2C_Master_Transmit( &hi, DEV_ADDR, (uint8_t *)recieve_buffer, 1, 100)!= HAL_OK){
-		return -7777;
+		return 0;
 	}
 
 	while(HAL_I2C_Master_Receive(&hi, new_addr, recieve_buffer, 2, 1000)!= HAL_OK){
-		   return -7777;
-		   //LCD_SetPos(8, 0);
-		   //LCD_String(str);
+		   return 0;
+
 	}
 	ads_value = (float)((recieve_buffer[0]<<8) + recieve_buffer[1]) * precision/1000;
 	return ads_value;
