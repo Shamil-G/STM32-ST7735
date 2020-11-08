1d67efef6f4e2ca0d451bb6183841e5696bccfee
Shamil Gusseynov <42405526+Shamil-G@users.noreply.github.com>
2020-11-08 14:51:27 +0600

    Add files via upload

--- ads1115.c
+++ ads1115.c
@@ -6,7 +6,6 @@
 #endif
 
 #include "ADS1115.h"
-uint16_t ads_value;
 float  precision;
 
 uint8_t cmd_value[] = { APR_CONF_REG, // select config register
@@ -29,6 +28,7 @@ int8_t init_ads1115(I2C_HandleTypeDef hi, uint8_t DEV_ADDR){
 	// second byte - select Config register - 0x01 = APR_CONF_REG
 	// third byte  - mode conversion
 	// fourth byte - SPS  and COMPARATOR mode
+	// to remember precision coefficient now
 	precision = ((cmd_value[1] & 0x0f)==UP_6V)?187.5:
 			    ((cmd_value[1] & 0x0f)==UP_4V)?125:
 			    ((cmd_value[1] & 0x0f)==UP_2V)?62.5:
@@ -43,11 +43,10 @@ int8_t init_ads1115(I2C_HandleTypeDef hi, uint8_t DEV_ADDR){
 }
 
 int16_t read_value_ads1115(I2C_HandleTypeDef hi, uint8_t DEV_ADDR){
-	// Select CHIP where DEV_ADDR select from ADDR_A0, ADDR_A1, ADDR_A2, ADDR_A3
-	// and select Conversation register
-	ads_value=0;
-	unsigned char recieve_buffer[2];
+	// For data to receive, DEV_ADDR must be increment on one
 	uint16_t new_addr = (uint16_t)DEV_ADDR | 0x01;
+	uint16_t ads_value=0;
+	unsigned char recieve_buffer[2];
 
 	recieve_buffer[0]=0x00;
 	while(HAL_I2C_Master_Transmit( &hi, DEV_ADDR, (uint8_t *)recieve_buffer, 1, 100)!= HAL_OK){
@@ -59,6 +58,7 @@ int16_t read_value_ads1115(I2C_HandleTypeDef hi, uint8_t DEV_ADDR){
 
 	}
 	ads_value = (float)((recieve_buffer[0]<<8) + recieve_buffer[1]) * precision/1000;
+	// result will be in mV
 	return ads_value;
 }
 
